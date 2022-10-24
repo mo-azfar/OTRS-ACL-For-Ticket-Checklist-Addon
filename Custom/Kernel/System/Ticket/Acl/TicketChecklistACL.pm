4c71365d4f302b69facaa381c7d3a32c63274fdd
Mohd Azfar <62957738+mo-azfar@users.noreply.github.com>
2022-10-24 09:54:40 +0800

    update code

--- Custom/Kernel/System/Ticket/Acl/TicketChecklistACL.pm
+++ Custom/Kernel/System/Ticket/Acl/TicketChecklistACL.pm
@@ -1,13 +1,15 @@
+# --
+# Copyright (C) 2022 mo-azfar, https://github.com/mo-azfar
+#
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
-# the enclosed file COPYING for license information (GPL). If you
-# did not receive this file, see https://www.gnu.org/licenses/gpl-3.0.txt.
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 # --
 
 package Kernel::System::Ticket::Acl::TicketChecklistACL;
 
 use strict;
 use warnings;
-use Data::Dumper;
 
 our @ObjectDependencies = (
     'Kernel::System::Log',
@@ -39,70 +41,61 @@ sub Run {
     }
 
     return 1 if !$Param{TicketID} || !$Param{UserID};
-    my @ChecklistEndState = split /;/, $Param{Config}->{ChecklistEndState};
-    my @NotPossibleTicketStates = split /;/, $Param{Config}->{NotPossibleTicketState};
-    my $NotPossibleDynamicFieldName = $Param{Config}->{NotPossibleDynamicFieldName};
-	my @NotPossibleDynamicFieldValue = split /;/, $Param{Config}->{NotPossibleDynamicFieldValue};
-    
-    #For generating acl unique name
-    my $random = int rand(8888);
-    my $ACLName = 'ACLTC_'.$random.'_'.$Param{Config}->{Name};
-
-    my $TicketChecklistObject = $Kernel::OM->Get('Kernel::System::PerlServices::TicketChecklist');
+	return 1 if !$Param{Config}->{ACLName};
+	return 1 if !@{$Param{Config}->{ChecklistEndState}};
+	return 1 if !@{$Param{Config}->{NotPossibleTicketState}};
+	
+	my $TicketChecklistObject = $Kernel::OM->Get('Kernel::System::PerlServices::TicketChecklist');
     my $TicketChecklistStatusObject = $Kernel::OM->Get('Kernel::System::PerlServices::TicketChecklistStatus');
 
+	#getting task list
     my @TASKLIST = $TicketChecklistObject->TicketChecklistTicketGet(
         TicketID => $Param{TicketID},
     );
+	
+	my $ACLGenerate = 0;
+    ACL:
+	
+	foreach my $Task (@TASKLIST)
+	{
+		my %TicketChecklistStatusData = $TicketChecklistStatusObject->TicketChecklistStatusGet( ID => $Task->{StatusID} );
+		
+		if ( grep( /^$TicketChecklistStatusData{Name}$/ , @{$Param{Config}->{ChecklistEndState}} ) ) {
+			#print "found it $TicketChecklistStatusData{Name}\n";
+			$ACLGenerate = 0;
+		}
+		else
+		{
+			$ACLGenerate = 1;
+			last ACL;
+		}
+			
+	}
 
-    
-    if (@TASKLIST)
-    {
-        my $count = 0;
-        my @ChecklistCurrentState;
+	if ($ACLGenerate)
+	{
+		$Param{Acl}->{$Param{Config}->{ACLName}} = {
         
-        foreach my $Task (@TASKLIST)
-        {
-        my %TicketChecklistStatusData = $TicketChecklistStatusObject->TicketChecklistStatusGet( ID => $Task->{StatusID} );
-        push @ChecklistCurrentState,  $TicketChecklistStatusData{Name};
-        $count ++;
-        }
+            # match properties
+            Properties => {
         
-        my %original = ();
-        my @isect = ();
-        map { $original{$_} = 1 } @ChecklistEndState;
-        @isect = grep { $original{$_} } @ChecklistCurrentState;
-        
-        #total number of matching checklist state not same as total checklist 
-        if (scalar(@isect) ne $count)
-        {
-            $Param{Acl}->{$ACLName} = {
-            
-                # match properties
-                Properties => {
-            
-                    # current ticket match properties
-                    Ticket => {
-                        TicketID => [ $Param{TicketID} ],
-                    },
-                    
+                # current ticket match properties
+                Ticket => {
+                    TicketID => [ $Param{TicketID} ],
                 },
-            
-                # return possible options (black list)
-                PossibleNot => {
-            
-                    # possible ticket options (black list)
-                    Ticket => {
-                        'DynamicField_'.$NotPossibleDynamicFieldName => [@NotPossibleDynamicFieldValue],
-                        State => [@NotPossibleTicketStates],
-                    },
+                
+            },
+        
+            # return possible options (black list)
+            PossibleNot => {
+        
+                # possible ticket options (black list)
+                Ticket => {
+                    State => [@{$Param{Config}->{NotPossibleTicketState}}],
                 },
-            };
-        
-        }
-        
-        return 1;
-    }
+            },
+        };
+	}
 
     return 1;
 }
